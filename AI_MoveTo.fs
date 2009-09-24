aeca28f5bfaf80a3577f7923cbaa0b9a85dbad75
Erik Schulz <gradbot@gmail.com>
2009-09-23 20:51:01 -0700

    AI now moves to selected location, enabled optimize and tailcalls in debug

---
+++ AI_MoveTo.fs
@@ -0,0 +1,42 @@
+﻿
+open Microsoft.Xna.Framework
+
+open Entity
+open Physical
+open Stats
+open Tree
+open AI
+
+type AI_MoveTo(entity : Entity, bind : AI_MoveTo -> unit) as self =
+    inherit AI()
+    
+    let mutable destination = Vector2()
+    
+    do bind self
+
+    member this.Destination
+        with get() = destination 
+        and set x = destination <- x
+        
+    member this.setDestination x =
+        destination <- x
+        this.Start()
+    
+    override this.Update(gameTime) =
+        if this.Running then
+            let distance = Vector2.Distance(destination, entity.Physical.Position)
+            let direction = (destination - entity.Physical.Position) / distance
+            
+            if entity.Stats.MaxVelocity * gameTime > distance then
+                entity.Physical.Position <- destination
+                entity.Physical.Velocity <- Vector2()//direction * distance / gameTime
+                base.Stop()
+            else
+                entity.Physical.Velocity <- direction * entity.Stats.MaxVelocity
+            
+    override this.Priority() =
+        if this.Running then
+            float <| Vector2.Distance(destination, entity.Physical.Position)
+        else
+            0.0
+            
--- Stage_1.fs
+++ Stage_1.fs
@@ -14,6 +14,8 @@ open AI_Follow
 open AI_Choose
 open AI_Attack
 open AI_Predicate
+open AI_MoveTo
+open Mouse
 
 type Stage_1(resource) =
     inherit Stage(resource)
@@ -23,8 +25,9 @@ type Stage_1(resource) =
     let trigger, event = Event.create()
     
     do  let obj1 = Entity(Physical(Vector2(), 1.0f, circle), Gear(), Stats.Default, Good)
-        let obj2 = Entity(Physical_Mouse(0.5f, circle), Gear(), Stats.Default, Evil)
+        let obj2 = Entity(Physical(Vector2(), 0.5f, circle), Gear(), Stats.Double, Evil)
         base.Entities <- [obj1; obj2]
+        
         let ai1 = 
             AI_Priority(
                 [
@@ -32,15 +35,16 @@ type Stage_1(resource) =
                     (AI_Attack(obj1, obj2) :> AI);
                 ])
         
-        // I'm just tossing around some ideas.
-        // I don't think I'm going to implement a scripting language for AI
         let ai2 =
-            AI_Predicate(
+            AI_Priority(
                 [
-                    (obj1.CompareDistance (<) obj2 0.2f, 
-                     (AI_Follow(obj1, obj2, Static(0.001)) :> AI));
+                    (AI_MoveTo(obj2, (fun this -> Mouse.LeftButton.Add(this.setDestination))) :> AI);
                 ])
         
-        base.Ais <- [(ai1 :> AI)]
+        base.Ais <- 
+            [
+            (ai1 :> AI);
+            (ai2 :> AI);
+            ]
         
 
