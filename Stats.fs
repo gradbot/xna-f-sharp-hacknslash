f6c0a912f2a97d80c4f31b3d1ccb4774d4d2915c
Erik Schulz <gradbot@gmail.com>
2009-09-23 20:48:22 -0700

    added double default to stats

--- Stats.fs
+++ Stats.fs
@@ -1,11 +1,15 @@
 ﻿
-type Stats(_healthPoints : int, level : int) =
+type Stats(_healthPoints : int, level : int, maxVelocity : float32) =
     let mutable healthPoints = _healthPoints
     
-    member this.MaxVelocity = 50.0f
+    member this.MaxVelocity = maxVelocity
     
     member this.Alive 
         with get() = healthPoints > 0
         
     static member Default =
-        Stats(100, 1)
+        Stats(100, 1, 50.0f)
+        
+    static member Double =
+        Stats(100, 1, 100.0f)
+
